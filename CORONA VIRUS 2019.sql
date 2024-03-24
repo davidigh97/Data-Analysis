7f6b7600d6050cda50d51eb64a23486ccff21835
davidigh97 <65161668+davidigh97@users.noreply.github.com>
2024-03-24 21:32:42 +0100

    Add files via upload

---
+++ CORONA VIRUS 2019.sql
@@ -0,0 +1,14 @@
+SELECT * 
+FROM `sql covid portfolio`.`covid deaths`;  
+SELECT location, date, total_cases, new_cases, total_deaths, population
+FROM `sql covid portfolio`. `covid deaths`;
+SELECT location, date, total_cases, total_deaths, (total_cases/total_deaths) as Deathpercentage 
+FROM `sql covid portfolio`. `covid deaths`ORDER BY 1,2;
+SELECT location, date, total_cases, population, (total_cases/population)*100 as Deathpercentage 
+FROM`sql covid portfolio`.`covid deaths`
+WHERE location LIKE '%states%'
+ORDER BY 1,2 
+SELECT location, population, MAX(total_cases)as Highestinfectioncount, MAX((total_cases/population))*100 as populationinfected
+FROM`sql covid portfolio`.`covid deaths`WHERE location like '%states' 
+group by population,location
+order by 1,2;
